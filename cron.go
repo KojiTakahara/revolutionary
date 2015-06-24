1b0648e0d2edc1225534df3a364950cb248e95aa
KojiTakahara <plateau1205@gmail.com>
2015-06-24 18:57:09 +0900

    fix

--- cron.go
+++ cron.go
@@ -4,34 +4,47 @@ import (
 	"appengine"
 	"appengine/datastore"
 	"appengine/urlfetch"
-	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
+	"math"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 )
 
+const vaultUrl = "http://dmvault.ath.cx/duel/tournament_history.php?tournamentId="
+
 func CreateTournamentHistory(r render.Render, params martini.Params, w http.ResponseWriter, req *http.Request) {
 	tournamentId, _ := strconv.Atoi(params["tournamentId"])
-
-	t, _ := time.Parse("2006-01-02", "2014-12-31")
-	d := now().Sub(t)
-	fmt.Println(d)
-
+	if tournamentId <= 0 {
+		t, _ := time.Parse("2006-01-02", "2007-04-11")
+		d := now().Sub(t)
+		tournamentId = int(math.Floor(d.Hours()/24)) + tournamentId
+	}
 	scrapingVault(tournamentId, req)
 	r.JSON(200, "")
 }
 
 func scrapingVault(id int, req *http.Request) {
-	url := "http://dmvault.ath.cx/duel/tournament_history.php?tournamentId=" + strconv.Itoa(id)
 	c := appengine.NewContext(req)
-	now := now()
 	client := urlfetch.Client(c)
-	resp, _ := client.Get(url)
+	resp, _ := client.Get(vaultUrl + strconv.Itoa(id))
 	doc, _ := goquery.NewDocumentFromResponse(resp)
+
+	info := doc.Find("#rightContainer p").Text()
+	runes := []rune(info)
+	date := now()
+	switch len(info) {
+	case 94:
+		date = stringToTime(string(runes[5:20]))
+	case 95:
+		date = stringToTime(string(runes[5:21]))
+	case 96:
+		date = stringToTime(string(runes[5:22]))
+	}
+
 	winPlayers := []string{}
 	loop := true
 	gameCount := 1
@@ -43,6 +56,7 @@ func scrapingVault(id int, req *http.Request) {
 		}
 		gameCount++
 	}
+
 	doc.Find(".player").Each(func(_ int, s *goquery.Selection) {
 		a := s.Find("a")
 		playerName := a.Text()
@@ -52,7 +66,7 @@ func scrapingVault(id int, req *http.Request) {
 			history.PlayerName = playerName
 			playerLink, _ := a.Attr("href")
 			history.PlayerId = strings.Trim(playerLink, "/author/")
-			history.Date = now
+			history.Date = date
 			s.Find(".civilcube").Each(func(_ int, s *goquery.Selection) {
 				color := s.Text()
 				if color == "光" {
@@ -76,7 +90,7 @@ func scrapingVault(id int, req *http.Request) {
 			history.Type = deckTypes[0]
 			history.Win = countWin(playerName, winPlayers)
 
-			keyStr := now.Format("20060102") + "_" + history.PlayerId
+			keyStr := date.Format("20060102") + "_" + history.PlayerId
 			key := datastore.NewKey(c, "TournamentHistory", keyStr, 0, nil)
 			_, err := datastore.Put(c, key, history)
 			if err != nil {
--- util.go
+++ util.go
@@ -9,3 +9,7 @@ const location = "Asia/Tokyo"
 func now() time.Time {
 	return time.Now().Local().In(time.FixedZone(location, 9*60*60))
 }
+
+func stringToTime(s string) time.Time {
+	return now()
+}
